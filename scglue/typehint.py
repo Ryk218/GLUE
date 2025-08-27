c5ca1e48dba96fc704eb00bdca468901a7efc269
Ryk218 <116294559+Ryk218@users.noreply.github.com>
2025-08-26 23:42:27 -0400

    Update typehint.py

--- scglue/typehint.py
+++ scglue/typehint.py
@@ -10,11 +10,28 @@ import numpy as np
 import scipy.sparse
 
 try:
+    # AnnData ≥ 0.12: public location
     from anndata.io import CSRDataset, CSCDataset
     SparseDataset = Union[CSRDataset, CSCDataset]
+except Exception:
+    try:
+        # AnnData 0.10.x: public (experimental) location
+        from anndata.experimental import CSRDataset, CSCDataset
+        SparseDataset = Union[CSRDataset, CSCDataset]
+    except Exception:
+        try:
+            # Very old AnnData: private names (best-effort)
+            from anndata._core.sparse_dataset import SparseDataset as _SparseDataset
+            SparseDataset = _SparseDataset
+        except Exception:
+            try:
+                from anndata._core.sparse_dataset import BaseCompressedSparseDataset as SparseDataset
+            except Exception:
+                # Last resort: don't crash over a type hint
+                class SparseDataset:  # type: ignore[no-redef]
+                    pass
+# --------------------------------------------------------------------
 
-except:
-    pass
 
 Array = Union[np.ndarray, scipy.sparse.spmatrix]
 BackedArray = Union[h5py.Dataset, SparseDataset]
